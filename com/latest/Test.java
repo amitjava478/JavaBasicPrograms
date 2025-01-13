77f292428cf0ba933fff30c3be59edcab069492e
amitjava478 <amitjava478@gmail.com>
2025-01-14 03:46:21 +0530

    Delete com directory

--- com/latest/Test.java
+++
@@ -1,9 +0,0 @@
-package com.latest;
-
-public class Test {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- com/latest/test738.java
+++
@@ -1,9 +0,0 @@
-package com.latest;
-
-public class test738 {
-
-	public static void main(String[] args) {
-
-	}
-
-}
